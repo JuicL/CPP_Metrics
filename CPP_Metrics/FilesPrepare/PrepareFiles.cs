74fe917285358ccf861392ad196c9332f7750ae8
JuicL <kolomin111@gmail.com>
2023-02-16 20:30:18 +0400

    iniy

--- CPP_Metrics/FilesPrepare/PrepareFiles.cs
+++ CPP_Metrics/FilesPrepare/PrepareFiles.cs
@@ -4,9 +4,26 @@ namespace CPP_Metrics.FilesPrepare
 {
     public class PrepareFiles
     {
-        // Путь до исходников
-        // Расширения файлов
-        // 
+        public string[] Extentions { get; } = new []{ ".cpp", ".h", ".hpp" };
+        public List<string> SourceFilesPath { get; } = new List<string>();
+        public Dictionary<string, FileInfo> Files { get; }
+        public PrepareFiles(List<string> sourceFilesPath)
+        {
+            SourceFilesPath = sourceFilesPath;
+            GetFileProject();
+        }
+        private void GetFileProject()
+        {
+            var files = DirectoiryFiles.GetFiles(SourceFilesPath, Extentions);
+            foreach (var item in files)
+            {
+                Files.Add(item.FullName,item);
+            }
+        }
 
+        public void HandleFile()
+        {
+
+        }
     }
 }
