f4793299e899b6921a9bf8374b82fb11c47dd68d
JuicL <kolomin111@gmail.com>
2023-04-26 18:36:22 +0400

    template

---
+++ CPP_Metrics/TemplateArgumentVisitor.cs
@@ -0,0 +1,14 @@
+﻿using Antlr4.Runtime.Tree;
+
+namespace CPP_Metrics
+{
+    public class TemplateArgumentVisitor : CPP14ParserBaseVisitor<bool>
+    {
+
+
+        public override bool VisitChildren(IRuleNode node)
+        {
+            return true;
+        }
+    }
+}
