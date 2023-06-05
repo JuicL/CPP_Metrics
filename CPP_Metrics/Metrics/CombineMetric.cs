963ad0699030ffea8da78374150d620d86d983dc
JuicL <kolomin111@gmail.com>
2023-06-06 00:36:29 +0400

    change general page

--- CPP_Metrics/Metrics/CombineMetric.cs
+++ CPP_Metrics/Metrics/CombineMetric.cs
@@ -10,13 +10,19 @@ namespace CPP_Metrics.Metrics
 {
 
 
-    public class CombineMetric : IMetric
+    public class CombineMetric : ICombineMetric
     {
-        public CombineMetric(List<IMetric> metrics)
-        {
-            
-        }
+        //I = Ce / (Ce + Ca)
 
+        // A
+        //
+        //      I(Instability)
+
+        //D = A + I - 1 
+        private CaCeMetric? CaCeMetric;
+        private ClassAbstraction? ClassAbstraction;
+        public Dictionary<string, decimal> Instability { get; set; } = new();
+      
         public IReportBuilder ReportBuilder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public List<MetricMessage> Messages { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
@@ -29,10 +35,30 @@ namespace CPP_Metrics.Metrics
         {
             throw new NotImplementedException();
         }
+        public bool Handle(List<IMetric> metrics)
+        {
+            CaCeMetric = (CaCeMetric?)metrics.SingleOrDefault(x => x is CaCeMetric);
+            ClassAbstraction = (ClassAbstraction?)metrics.SingleOrDefault(x => x is ClassAbstraction);
+            if (CaCeMetric == null || ClassAbstraction == null) return false;
+
+            foreach (var Ce in CaCeMetric.Ce)
+            {
+                CaCeMetric.Ca.TryGetValue(Ce.Key,out int ca);
+
+            }
+
+
+
+
+
+
+            return true;
+        }
 
         public bool Handle(ProcessingFileInfo processingFileInfo)
         {
             throw new NotImplementedException();
         }
+
     }
 }
